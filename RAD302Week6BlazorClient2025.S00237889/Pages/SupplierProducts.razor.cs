d593b43283e18dc382746141f74ddadc22817706
Ryan <s00237889@atu.ie>
2025-04-02 16:41:02 +0100

    Week 6 Lab Supplier Product View Setup and Lab finished S00237889

---
+++ RAD302Week6BlazorClient2025.S00237889/Pages/SupplierProducts.razor.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using RAD302Week6BlazorClient2025.S00237889;
+using Rad302Week6Lab2025.DataModel.S00237889;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RAD302Week6BlazorClient2025.S00237889.Pages
+{
+    public partial class SupplierProducts
+    {
+        [Parameter]
+        public int SupplierID { get; set; }
+        public Supplier Current { get; set; }
+
+
+
+        protected override Task OnInitializedAsync()
+        {
+            Current = SupplierStaticContext.GetNewSupplier(SupplierID);
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
--- RAD302Week6BlazorClient2025.S00237889/Program.cs
+++ RAD302Week6BlazorClient2025.S00237889/Program.cs
@@ -1,11 +1,33 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Rad302Week6Lab2025.DataModel.S00237889;
+using RAD302Week6BlazorClient2025.S00237889.Pages;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using RAD302Week6BlazorClient2025.S00237889;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+namespace RAD302Week6BlazorClient2025.S00237055;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        builder.RootComponents.Add<App>("#app");
+        //builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        await builder.Build().RunAsync();
+
+
+    }
+}
