3f8e8a030dacf5d2b96e088c4356f0c28632eeb9
Ryan <s00237889@atu.ie>
2025-04-02 16:34:08 +0100

    Week 6 Lab Supplier View Setup S00237889

---
+++ RAD302Week6BlazorClient2025.S00237889/Pages/SupplierView.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Rad302Week6Lab2025.DataModel.S00237889;
+using System.Net.Http.Json;
+
+namespace RAD302Week6BlazorClient2025.S00237889.Pages
+{
+    public partial class SupplierView
+    {
+
+        [Inject]
+        HttpClient Http { get; set; }
+
+        private Supplier[] suppliers;
+        private Product[] products;
+
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (SupplierStaticContext.Suppliers == null)
+            {
+                SupplierStaticContext.Suppliers =
+                        suppliers = await Http.GetFromJsonAsync<Supplier[]>("sample-data/Supplier.json");
+                SupplierStaticContext.Products = products = await Http.GetFromJsonAsync<Product[]>("sample-data/Product.json");
+                InitialiseSupplierProducts();
+            }
+            else
+            {
+                suppliers = SupplierStaticContext.Suppliers;
+            }
+            await base.OnInitializedAsync();
+        }
+
+        private void InitialiseSupplierProducts()
+        {
+            foreach (var item in suppliers)
+            {
+                List<Product> toadd =
+                    products.OrderBy(o => new Random().Next(2372))
+                    .Take(3).ToList();
+                item.SupplierProducts = new List<Product>();
+                item.SupplierProducts.AddRange(toadd);
+
+
+            }
+
+        }
+    }
+
+}
---
+++ RAD302Week6BlazorClient2025.S00237889/SupplierStaticContext.cs
@@ -0,0 +1,16 @@
+﻿using Rad302Week6Lab2025.DataModel.S00237889;
+
+namespace RAD302Week6BlazorClient2025.S00237889
+{
+    public class SupplierStaticContext
+    {
+        public static Supplier CurrentSupplier { get; set; }
+        public static Product[] Products { get; set; }
+        public static Supplier[] Suppliers { get; set; }
+        public static Supplier GetNewSupplier(int supplierID)
+        {
+            return CurrentSupplier = Suppliers.FirstOrDefault(s => s.SupplierID == supplierID);
+        }
+    }
+
+}
